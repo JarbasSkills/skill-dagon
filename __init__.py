245ac13c5228dceb0eee736e0851268861954be6
jarbasal <jarbasai@mailfence.com>
2020-12-30 00:06:12 +0000

    ovos_utils

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ class DagonSkill(CommonPlaySkill):
     def __init__(self):
         super().__init__("Dagon")
         if "download_audio" not in self.settings:
-            self.settings["download_audio"] = True
+            self.settings["download_audio"] = False
         if "download_video" not in self.settings:
             self.settings["download_video"] = False
         if "audio_only" not in self.settings:
@@ -26,14 +26,17 @@ class DagonSkill(CommonPlaySkill):
     def initialize(self):
         self.add_event('skill-dagon.jarbasskills.home',
                        self.handle_homescreen)
-        if self.settings["download_audio"]:
-            self.get_audio_stream(download=True)
-        # if self.settings["download_video"]:
-        #    self.get_video_stream(download=True)
+        try:
+            if self.settings["download_audio"]:
+                self.get_audio_stream(download=True)
+            if self.settings["download_video"]:
+                self.get_video_stream(download=True)
+        except:
+            pass
 
     def get_intro_message(self):
         self.speak_dialog("intro")
-        self.gui.show_image(join(dirname(__file__), "ui", "bg.png"))
+        self.gui.show_image(join(dirname(__file__), "ui", "dagon.png"))
 
     # homescreen
     def handle_homescreen(self, message):
@@ -58,6 +61,7 @@ class DagonSkill(CommonPlaySkill):
     def clean_vocs(self, phrase):
         phrase = self.remove_voc(phrase, "reading")
         phrase = self.remove_voc(phrase, "lovecraft")
+        phrase = self.remove_voc(phrase, "video")
         phrase = self.remove_voc(phrase, "audio_theatre")
         phrase = self.remove_voc(phrase, "play")
         phrase = phrase.strip()
@@ -73,27 +77,41 @@ class DagonSkill(CommonPlaySkill):
             score += 0.1
             match = CPSMatchLevel.GENERIC
         elif media_type == CPSMatchType.VIDEO:
-            score += 0.2
+            score += 0.15
             match = CPSMatchLevel.GENERIC
         elif media_type == CPSMatchType.VISUAL_STORY:
             score += 0.3
             match = CPSMatchLevel.CATEGORY
 
+        if self.voc_match(original, "reading"):
+            score += 0.1
+            match = CPSMatchLevel.GENERIC
+
         if self.voc_match(original, "audio_theatre"):
-            score += 0.15
+            score += 0.1
             match = CPSMatchLevel.CATEGORY
 
-        if self.voc_match(original, "lovecraft"):
-            score += 0.5
-            match = CPSMatchLevel.ARTIST
+        if self.voc_match(original, "video"):
+            score += 0.1
+            match = CPSMatchLevel.CATEGORY
 
         phrase = self.clean_vocs(phrase)
 
+        if self.voc_match(phrase, "lovecraft"):
+            score += 0.3
+            match = CPSMatchLevel.ARTIST
+            if self.voc_match(original, "video"):
+                score += 0.1
+                match = CPSMatchLevel.MULTI_KEY
+
         if self.voc_match(phrase, "dagon"):
             score += 0.75
-            match = CPSMatchLevel.TITLE
+            if match is not None:
+                match = CPSMatchLevel.MULTI_KEY
+            else:
+                match = CPSMatchLevel.TITLE
 
-        if score >= 0.85:
+        if score >= 0.9:
             match = CPSMatchLevel.EXACT
 
         if match is not None:
@@ -107,7 +125,12 @@ class DagonSkill(CommonPlaySkill):
         url = "https://www.youtube.com/watch?v=Gv1I0y6PHfg"
         if self.gui.connected and not self.settings["audio_only"]:
             url = self.get_video_stream(url, self.settings["download_video"])
-            self.gui.play_video(url)
+            self.CPS_send_status(uri=url,
+                                 image=image,
+                                 background_image=bg,
+                                 playlist_position=0,
+                                 status=CPSTrackStatus.PLAYING_GUI)
+            self.gui.play_video(url, "Dagon , by H. P. Lovecraft")
         else:
             url = self.get_audio_stream(url, self.settings["download_audio"])
             self.audioservice.play(url, utterance=self.play_service_string)
@@ -118,14 +141,13 @@ class DagonSkill(CommonPlaySkill):
                                  status=CPSTrackStatus.PLAYING_AUDIOSERVICE)
 
     def stop(self):
-        self.gui.clear()
+        self.gui.release()
 
     # youtube handling
     @staticmethod
     def get_audio_stream(url="https://www.youtube.com/watch?v=Gv1I0y6PHfg",
                          download=False):
-        myvid = pafy.new(url)
-        stream = myvid.getbestaudio()
+        stream = pafy.new(url).getbestaudio()
 
         # TODO check if https supported, if not download=True without
         #  needing user changing settings.json
@@ -150,11 +172,14 @@ class DagonSkill(CommonPlaySkill):
     @staticmethod
     def get_video_stream(url="https://www.youtube.com/watch?v=Gv1I0y6PHfg",
                          download=False):
-        video = pafy.new(url)
-        playstream = video.streams[0]
-        # TODO dl video ?
-        # not worth caching
-        return playstream.url
+        stream = pafy.new(url).streams[0]
+        if download:
+            path = join(gettempdir(),
+                        url.split("watch?v=")[-1] + "." + stream.extension)
+            if not exists(path):
+                stream.download(path)
+            return path
+        return stream.url
 
 
 def create_skill():
