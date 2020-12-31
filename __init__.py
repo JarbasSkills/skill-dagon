e6f4591e7647e06fa1c02ff636da1150c34cb5fd
jarbasal <jarbasai@mailfence.com>
2020-12-31 18:22:15 +0000

    use skill template

--- __init__.py
+++ __init__.py
@@ -1,71 +1,26 @@
-from ovos_utils.waiting_for_mycroft.common_play import \
-    CommonPlaySkill, CPSMatchLevel, CPSMatchType, CPSTrackStatus
-from ovos_utils import create_daemon
-from pyvod.utils import get_audio_stream, get_video_stream
-import re
+from ovos_utils.skills.templates.media_player import MediaSkill, \
+    CPSMatchType, CPSMatchLevel
 from os.path import join, dirname
 
 
-class DagonSkill(CommonPlaySkill):
+class DagonSkill(MediaSkill):
 
     def __init__(self):
         super().__init__("Dagon")
-        if "download_audio" not in self.settings:
-            self.settings["download_audio"] = False
-        if "download_video" not in self.settings:
-            self.settings["download_video"] = False
-        if "audio_only" not in self.settings:
-            self.settings["audio_only"] = False
-        if "mp3_audio" not in self.settings:
-            self.settings["mp3_audio"] = True
         self.supported_media = [CPSMatchType.GENERIC,
                                 CPSMatchType.AUDIOBOOK,
                                 CPSMatchType.VISUAL_STORY,
                                 CPSMatchType.VIDEO]
         self.default_bg = join(dirname(__file__), "ui", "bg.png")
         self.default_image = join(dirname(__file__), "ui", "logo.png")
-
-    def initialize(self):
-        self.add_event('skill-dagon.jarbasskills.home',
-                       self.handle_homescreen)
-        create_daemon(self._bootstrap)
-
-    def _bootstrap(self):
-        # bootstrap, so data is cached
-        url = "https://www.youtube.com/watch?v=Gv1I0y6PHfg"
-        try:
-            if self.settings["download_audio"]:
-                get_audio_stream(url, download=True,
-                                 to_mp3=self.settings["mp3_audio"])
-            if self.settings["download_video"]:
-                get_video_stream(url, download=True)
-        except:
-            pass
+        self.message_namespace = 'skill-dagon.jarbasskills.home'
+        self.bootstrap_list = ["https://www.youtube.com/watch?v=Gv1I0y6PHfg"]
 
     def get_intro_message(self):
         self.speak_dialog("intro")
         self.gui.show_image(join(dirname(__file__), "ui", "dagon.png"))
 
-    # homescreen
-    def handle_homescreen(self, message):
-        self.CPS_start("dagon", {"media_type": CPSMatchType.VIDEO})
-
     # common play
-    def remove_voc(self, utt, voc_filename, lang=None):
-        lang = lang or self.lang
-        cache_key = lang + voc_filename
-
-        if cache_key not in self.voc_match_cache:
-            self.voc_match(utt, voc_filename, lang)
-
-        if utt:
-            # Check for matches against complete words
-            for i in self.voc_match_cache[cache_key]:
-                # Substitute only whole words matching the token
-                utt = re.sub(r'\b' + i + r"\b", "", utt)
-
-        return utt
-
     def clean_vocs(self, phrase):
         phrase = self.remove_voc(phrase, "reading")
         phrase = self.remove_voc(phrase, "lovecraft")
@@ -128,33 +83,6 @@ class DagonSkill(CommonPlaySkill):
                      "image": self.default_image, "background": self.default_bg,
                      "stream": "https://www.youtube.com/watch?v=Gv1I0y6PHfg"})
         return None
-
-    def CPS_start(self, phrase, data):
-        bg = data.get("background") or self.default_bg
-        image = data.get("image") or self.default_image
-        url = data.get("stream")
-        if self.gui.connected and not self.settings["audio_only"]:
-            url = get_video_stream(url,
-                                   download=self.settings["download_video"])
-            self.CPS_send_status(uri=url,
-                                 image=image,
-                                 background_image=bg,
-                                 playlist_position=0,
-                                 status=CPSTrackStatus.PLAYING_GUI)
-            self.gui.play_video(url, "Dagon , by H. P. Lovecraft")
-        else:
-            url = get_audio_stream(url,
-                                   download=self.settings["download_audio"],
-                                   to_mp3=self.settings["mp3_audio"])
-            self.audioservice.play(url, utterance=self.play_service_string)
-            self.CPS_send_status(uri=url,
-                                 image=image,
-                                 background_image=bg,
-                                 playlist_position=0,
-                                 status=CPSTrackStatus.PLAYING_AUDIOSERVICE)
-
-    def stop(self):
-        self.gui.release()
 
 
 def create_skill():
