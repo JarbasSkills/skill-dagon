7ca1f3891a93a84e494aeda57365fd1661a12359
jarbasal <jarbasai@mailfence.com>
2021-10-13 22:58:47 +0100

    ocp playlists

--- README.md
+++ README.md
@@ -1,29 +1,13 @@
-# <img src='./res/icon/dagon.png' card_color='#40DBB0' width='50' height='50' style='vertical-align:bottom'/> Dagon, A Lovecraft Story Skill 
+# <img src='./res/icon/dagon.png' card_color='#40DBB0' width='50' height='50' style='vertical-align:bottom'/> Lovecraft Comics Skill
 
-Adaption of Dagon, by H. P. Lovecraft
+Compilation of Comic adaptations of stories by H. P. Lovecraft
 
 ![](./ui/logo.png)
 
 
-## About 
-
-This skill brings to you an adaption of Dagon, by H. P. Lovecraft
-
-An illustrated reading of 'Dagon' by H.P. Lovecraft. Read by Mike Bennett, illustrated by Christopher Steininger.
-"Dagon" is a short story by American author H. P. Lovecraft. It was written in July 1917 and is one of the first stories that Lovecraft wrote as an adult. It was first published in the November 1919 edition of The Vagrant. Dagon was later published in Weird Tales.
-
-Illustrated by Christopher Edwin Steininger
-
-![](./gui2.gif)
-![](./gui.gif)
-
 ## Examples 
 
-* "read dagon"
-* "play lovecraft video"
-* "play dagon"
-* "play dagon audio drama"
-* "play the dagon video"
+* "play lovecraft comics"
 
 # Platform support
 
@@ -50,8 +34,8 @@ Illustrated by Christopher Edwin Steininger
 
 ## Credits 
 - JarbasAl
-- [DAGON by H. P. Lovecraft (youtube)](https://www.youtube.com/watch?v=Gv1I0y6PHfg)
-
+- Tanabe Gou
+- Jeremy Zahn
 
 ## Category
 **Entertainment**
@@ -59,6 +43,7 @@ Illustrated by Christopher Edwin Steininger
 ## Tags
 #audio 
 #books
+#comics
 #radio theatre
 #lovecraft
 #entertainment
--- __init__.py
+++ __init__.py
@@ -1,56 +1,36 @@
-from ovos_utils import create_daemon
-from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill
-from ovos_workshop.frameworks.playback import CommonPlayMediaType, CommonPlayPlaybackType, \
-    CommonPlayMatchConfidence
-import re
 from os.path import join, dirname
 
+from json_database import JsonStorage
+from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
+    MediaType, PlaybackType, ocp_search
 
-class DagonSkill(OVOSCommonPlaybackSkill):
+
+class LovecraftComicsSkill(OVOSCommonPlaybackSkill):
 
     def __init__(self):
-        super().__init__("Dagon")
-        self.supported_media = [CommonPlayMediaType.GENERIC,
-                                CommonPlayMediaType.AUDIOBOOK,
-                                CommonPlayMediaType.VISUAL_STORY,
-                                CommonPlayMediaType.VIDEO]
+        super().__init__("LovecraftComics")
+        self.supported_media = [MediaType.GENERIC,
+                                MediaType.AUDIOBOOK,
+                                MediaType.VISUAL_STORY,
+                                MediaType.VIDEO]
         self.default_bg = join(dirname(__file__), "ui", "bg.png")
         self.default_image = join(dirname(__file__), "ui", "dagon.png")
-        self.skill_logo = join(dirname(__file__), "ui", "logo.png")
         self.skill_icon = join(dirname(__file__), "ui", "icon.png")
+        self.db = JsonStorage(join(dirname(__file__), "res",
+                                   "JeremyZahn_lovecraft_comics.json"))
+        self.db2 = JsonStorage(join(dirname(__file__), "res",
+                                    "TanabeGou_lovecraft_comics.json"))
 
-    # better common play
-    def CPS_search(self, phrase, media_type):
-        """Analyze phrase to see if it is a play-able phrase with this skill.
-
-        Arguments:
-            phrase (str): User phrase uttered after "Play", e.g. "some music"
-            media_type (CommonPlayMediaType): requested CPSMatchType to search for
-
-        Returns:
-            search_results (list): list of dictionaries with result entries
-            {
-                "match_confidence": CommonPlayMatchConfidence.HIGH,
-                "media_type":  CPSMatchType.MUSIC,
-                "uri": "https://audioservice.or.gui.will.play.this",
-                "playback": CommonPlayPlaybackType.VIDEO,
-                "image": "http://optional.audioservice.jpg",
-                "bg_image": "http://optional.audioservice.background.jpg"
-            }
-        """
-        # there is a single video in this skill, let's simply calculate a
-        # match score
-        original = phrase
+    def get_base_score(self, original, media_type):
         score = 0
-
-        if media_type == CommonPlayMediaType.AUDIOBOOK:
+        if media_type == MediaType.AUDIOBOOK:
             score += 10
-        elif media_type == CommonPlayMediaType.VIDEO:
+        elif media_type == MediaType.VIDEO:
             score += 5
-        elif media_type == CommonPlayMediaType.VISUAL_STORY:
+        elif media_type == MediaType.VISUAL_STORY:
             score += 30
 
-        if self.voc_match(original, "reading") or\
+        if self.voc_match(original, "reading") or \
                 self.voc_match(original, "audio_theatre"):
             score += 10
 
@@ -59,31 +39,71 @@ class DagonSkill(OVOSCommonPlaybackSkill):
             if self.voc_match(original, "video"):
                 score += 10
 
-        if self.voc_match(phrase, "dagon"):
-            score += 70
+        if self.voc_match(original, "comic"):
+            score += 10
+        return score
 
-        if score >= CommonPlayMatchConfidence.AVERAGE_LOW:
-            # returning both VIDEO and AUDIO options, better-playback-skill
-            # will select which one to play, a check for self.gui.connected
-            # in here introduces latency and penalizes this skill
-            return [
-                {
-                    "match_confidence": min(100, score),
-                    "media_type": CommonPlayMediaType.VISUAL_STORY,
-                    "uri": "https://www.youtube.com/watch?v=Gv1I0y6PHfg",
-                    "playback": CommonPlayPlaybackType.VIDEO,
-                    "image": self.default_image,
-                    "bg_image": self.default_bg,
-                    "skill_icon": self.skill_icon,
-                    "skill_logo": self.skill_logo,
-                    "title": "DAGON",
-                    "author": "H. P. Lovecraft",
-                    'length': 1135 * 1000
-                }]
-        return None
+    @ocp_search()
+    def ocp_motioncomics_lovecraft_playlist(self, phrase, media_type):
+        score = self.get_base_score(phrase, media_type)
+        if self.voc_match(phrase, "comic") or \
+                media_type == MediaType.VISUAL_STORY:
+            score += 10
+            if self.voc_match(phrase, "lovecraft"):
+                score += 30
+        pl = [
+            {
+                "match_confidence": score,
+                "media_type": MediaType.VISUAL_STORY,
+                "uri": entry["uri"],
+                "playback": PlaybackType.VIDEO,
+                "image": entry["image"],
+                "bg_image": self.default_bg,
+                "skill_icon": self.skill_icon,
+                "title": entry["title"],
+                "author": "H. P. Lovecraft",
+                "album": "Lovecraft Motion Comics by Jeremy Zahn"
+            } for title, entry in self.db.items()
+        ]
+        pl2 = [
+            {
+                "match_confidence": score,
+                "media_type": MediaType.VISUAL_STORY,
+                "uri": entry["uri"],
+                "playback": PlaybackType.VIDEO,
+                "image": entry["image"],
+                "bg_image": self.default_bg,
+                "skill_icon": self.skill_icon,
+                "title": entry["title"],
+                "author": "H. P. Lovecraft",
+                "album": "Lovecraft Illustrated by Tanabe Gou"
+            } for title, entry in self.db2.items()
+        ]
+
+        return [{
+            "match_confidence": score,
+            "media_type": MediaType.VISUAL_STORY,
+            "playlist": pl,
+            "playback": PlaybackType.VIDEO,
+            "skill_icon": self.skill_icon,
+            "image": "https://img.youtube.com/vi/Gv1I0y6PHfg/hqdefault.jpg",
+            "bg_image": self.default_image,
+            "title": "Lovecraft Comics by Jeremy Zahn (Playlist)",
+            "author": "H. P. Lovecraft",
+            "album": "Lovecraft Motion Comics by Jeremy Zahn"
+        }, {
+            "match_confidence": score,
+            "media_type": MediaType.VISUAL_STORY,
+            "playlist": pl2,
+            "playback": PlaybackType.VIDEO,
+            "skill_icon": self.skill_icon,
+            "image": "https://img.youtube.com/vi/jW_G7cGxCWY/hqdefault.jpg",
+            "bg_image": self.default_image,
+            "title": "Lovecraft Illustrated by Tanabe Gou (Playlist)",
+            "author": "H. P. Lovecraft",
+            "album": "Lovecraft Illustrated by Tanabe Gou"
+        }]
 
 
 def create_skill():
-    return DagonSkill()
-
-
+    return LovecraftComicsSkill()
